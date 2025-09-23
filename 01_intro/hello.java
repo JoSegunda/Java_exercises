800061c01c6e7c553a9b8397bd99f992ea3f2a7d
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-09-23 12:48:44 +0100

    Readme updated

--- 01_intro/hello.java
+++ 01_intro/hello.java
@@ -1,6 +1,7 @@
 public class hello{
     public static void main(String[] args){
 
-        System.out.println("Hello, World");
+        String name = "Joel";
+        System.out.println("Hello "+name);
     }
 }
