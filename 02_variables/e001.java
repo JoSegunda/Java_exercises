441f025a1f2e34ce42a2fb67e160d51113ebeb8b
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-15 15:27:04 +0100

    added exercise 004

--- 02_variables/e001.java
+++
@@ -1,9 +0,0 @@
-public class e001 {
-    public static void main(String[] args){
-
-        String name = "Lucas";
-        int idade = 28;
-
-        System.out.println("Eu me chamo "+name+" e tenho "+idade+" anos de idade");
-    }
-}
---
+++ Exercises/exe004.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class exe004 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Valor do cateto oposto: ");
+        double co = sc.nextDouble();
+
+        System.out.print("Valor do cateto oposto: ");
+        double ca = sc.nextDouble();
+
+        double hypo =  Math.hypot(ca,co);
+        System.out.println("Valor da hipotenusa: "+hypo);
+        sc.close();
+    }
+}
