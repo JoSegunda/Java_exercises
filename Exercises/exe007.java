23009d80aa4d266d1ec8db28ac9449f982b12515
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-20 16:45:28 +0100

    updated exe007

---
+++ Exercises/exe007.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class exe007 {
+    public static void main(String[] args){
+
+        Random gen = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-------------------------------");
+        System.out.println("Guessing number game [5 tries]");
+        System.out.println("-------------------------------");
+
+        int n = gen.nextInt(1,11);
+        int attempts = 5;
+        while (true){
+            System.out.print("Enter a Guess [1 - 10]: ");
+            int guess = sc.nextInt();
+
+
+            if (guess == n){
+                System.out.println("You got it right!\nI was thinking of " + n);
+                break;
+            }
+            if (attempts == 0){
+                System.out.println("You lost!\nI was thinking of "+ n);
+                break;
+            }
+            System.out.println((guess > n) ? "Too High":"Too Low");
+            attempts--;
+            System.out.println(attempts + " attempts left.");
+
+
+        }
+
+    }
+}
