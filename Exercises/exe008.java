d3f84c8cbc54cdf25527a52ef7486c934adc7562
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-22 14:43:27 +0100

    updated exercise 008

--- Exercises/exe008.java
+++ Exercises/exe008.java
@@ -3,36 +3,29 @@ import java.util.Scanner;
 public class exe008 {
     static double balance = 0;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("==========================");
-        System.out.println("      Banking program     ");
-        System.out.println("==========================");
-        System.out.println("|     1. Show balance    |");
-        System.out.println("|     2. deposit         |");
-        System.out.println("|     3. Withdraw        |");
-        System.out.println("|     4. Exit            |");
-        System.out.println("==========================");
-
         int choice;
-        double depositAmount, withDrawal;
+        double depositAmount, withdraw;
+
         do {
-            System.out.println("Enter your choice (1 - 4): ");
+            Thread.sleep(2000);
+            printMenu();
+            System.out.print("Enter your choice (1 - 4): ");
             choice = sc.nextInt();
 
             switch (choice){
                 case 1 -> ShowBalance();
                 case 2 -> {
-                    System.out.println("Deposit amount: ");
+                    System.out.print("Deposit amount: ");
                     depositAmount = sc.nextDouble();
                     deposit(depositAmount);
                 }
                 case 3 ->{
-                    System.out.println("Amount to withdraw: ");
-                    withDrawal = sc.nextDouble();
+                    System.out.print("Amount to withdraw: ");
+                    withdraw = sc.nextDouble();
+                    withdrawAmount(withdraw);
                 }
             }
         } while (choice != 4);
@@ -47,18 +40,37 @@ public class exe008 {
 
     static void deposit(double amount){
         printDashes();
+        balance = amount;
         System.out.println("Deposited $"+amount);
         printDashes();
     }
 
-    static  void withdrawalAmount(double amount){
-        balance -= amount;
-        printDashes();
-        System.out.println("Withdrawn $"+amount);
-        printDashes();
+    static  void withdrawAmount(double amount){
+        balance = balance - amount;
+        if (amount > balance){
+            System.out.println("Current Balance: "+balance);
+            System.out.println("Withdrawal too high.");
+
+        }else{
+            printDashes();
+            System.out.println("Withdrawn $"+amount);
+            printDashes();
+        }
+
     }
 
     static void printDashes(){
         System.out.println("-----------------------");
+    }
+
+    static void printMenu(){
+        System.out.println("==========================");
+        System.out.println("      Banking program     ");
+        System.out.println("==========================");
+        System.out.println("|     1. Show balance    |");
+        System.out.println("|     2. deposit         |");
+        System.out.println("|     3. Withdraw        |");
+        System.out.println("|     4. Exit            |");
+        System.out.println("==========================");
     }
 }
