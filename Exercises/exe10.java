7fed6b49149935eadc55bc8ce38e2f7319553cab
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-25 00:38:30 +0100

    added exercise 10

---
+++ Exercises/exe10.java
@@ -0,0 +1,17 @@
+public class exe10 {
+    public static void main(String[] args){
+
+        char[][] numbers = {
+                {'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'},
+                {'*','0','#'}};
+
+        for(char[] values: numbers){
+            for (char value : values){
+                System.out.print(value+" ");
+            }
+            System.out.println();
+        }
+    }
+}
