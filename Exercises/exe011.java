b017de4ba08f3953512ed0b9d13f11692b68f69f
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-25 00:54:00 +0100

    Added exercise 11

---
+++ Exercises/exe011.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exe011 {
+    public static void main(String[] args){
+
+        String[] questions ={
+                "What does a variable store", "Which symbol is used for comments in Python",
+                "What is the result of 3 + 2 * 2 in most programming languages","Which data type is used to store true or false values",
+                "What is the purpose of an if statement","Which of these is a loop structure","What is an algorithm"};
+        String[][] options= {
+                {"Only numbers","Only text","Data values that can change", "Information that can vary during program execution"},
+                {"//","#","<!-->","/* */"}, {"10","7","8","9"},{"int","String","boolean","float"},{"To repeat a block of code","To make decisions based on conditions","To import libraries","To define a variable"},
+                {"if","for","print","break"},{"A set of steps to solve a problem","A programming language","A data structure","A type of variable"}
+        };
+        System.out.println("============================");
+        System.out.println("Quiz Game");
+        System.out.println("============================");
+    }
+}
