230d6355b5e0496f141999224ea4bdea29b097c5
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-25 16:59:00 +0100

    updated exercise 11

--- Exercises/exe012.java
+++ Exercises/exe012.java
@@ -1,9 +1,34 @@
 import java.util.Scanner;
-
+import java.util.Random;
 public class exe012 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        String[] option = {"rock","paper","Scissors"};
+        int stop = 0;
 
+        System.out.println("==========================");
+        System.out.println("Rock, Paper and Scissors Game");
+        System.out.println("==========================");
+
+        String move;
+        int indexChoice;
+        while(true) {
+            System.out.print("Enter Your move: ");
+            move = sc.nextLine();
+
+            indexChoice = random.nextInt(0,4);
+            Thread.sleep(2000);
+            if (option[indexChoice].equals(move)){
+                System.out.println("==========");
+                System.out.println("You Won");
+                System.out.println("==========");
+            }
+
+            if (stop == 1){
+                break;
+            }
+        }
 
         sc.close();
     }
