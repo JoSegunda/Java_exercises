e39b3038e7cd19334d4893131e75e11769348445
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-26 19:44:20 +0000

    updated exercise 13

--- Exercises/exe013.java
+++ Exercises/exe013.java
@@ -4,7 +4,10 @@ import java.util.Random;
 public class exe013 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("===========================");
+        System.out.println("  Welcome to Java Slots");
+        System.out.println("Symbols: \uD83C\uDF52");
+        System.out.println("===========================");
 
         sc.close();
     }
