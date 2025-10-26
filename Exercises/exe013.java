d69b05bc65ac84a726b3808757b5063342237fe1
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-26 19:41:43 +0000

    updated exercise 13

--- Exercises/exe013.java
+++ Exercises/exe013.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class exe013 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+
+        sc.close();
     }
 }
