b1eba6f8eeff32c3488f0445188b7877ff3bac56
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-26 20:02:55 +0000

    updated exercise 13

--- Exercises/exe013.java
+++ Exercises/exe013.java
@@ -4,15 +4,24 @@ import java.util.Random;
 public class exe013 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        Random random = new Random();
         String[] symbols = {"🍒","🍉","🍋","🔔","🌟"};
         int balance = 100;
+        int bet = 0;
 
         System.out.println("===========================");
         System.out.println("  Welcome to Java Slots");
         System.out.println("Symbols: 🍒 🍉 🍋 🔔 🌟");
         System.out.println("===========================");
 
+        while (true){
+            System.out.println("Place your bet amount: ");
+            bet = sc.nextInt();
 
+            System.out.println("=========");
+
+            System.out.println("=========");
+        }
 
         sc.close();
     }
