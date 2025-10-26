91d0e0d1e21b95c3850e21b36a168b2b72cbdc03
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-26 20:08:05 +0000

    updated exercise 13

--- Exercises/exe013.java
+++ Exercises/exe013.java
@@ -8,20 +8,27 @@ public class exe013 {
         String[] symbols = {"🍒","🍉","🍋","🔔","🌟"};
         int balance = 100;
         int bet = 0;
+        String stop = "";
 
         System.out.println("===========================");
         System.out.println("  Welcome to Java Slots");
         System.out.println("Symbols: 🍒 🍉 🍋 🔔 🌟");
         System.out.println("===========================");
 
-        while (true){
+        do {
             System.out.println("Place your bet amount: ");
             bet = sc.nextInt();
 
             System.out.println("=========");
 
             System.out.println("=========");
-        }
+
+            while (!(stop.equals("y")) && !(stop.equals("n"))) {
+                System.out.print("Do you want to play again? [Y / N]");
+                stop = sc.nextLine().toLowerCase();
+            }
+
+        } while (!stop.equals("n"));
 
         sc.close();
     }
