8093a0f41503196725949b3387b06ff709e16b33
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-30 14:10:44 +0000

    updated readme

---
+++ Exercises/exe014.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class exe014 {
+    public static void main(String[] args){
+        ArrayList<String> fruits = new ArrayList<>();
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Number of fruits you want to save: ");
+        n = sc.nextInt();
+
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++){
+            System.out.print("Enter a fruit: ");
+            String fruit = sc.nextLine();
+
+            fruits.add(fruit);
+        }
+        System.out.println(fruits);
+        sc.close();
+    }
+}
