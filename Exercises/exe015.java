282e4b5b60757a491c7274d8886dc05e87984c33
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-03 13:11:51 +0000

    updated exe015

--- Exercises/exe015.java
+++ Exercises/exe015.java
@@ -1,7 +1,22 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class exe015 {
     public static void main(String[] args){
 
-        String filepath = "/home/joel-pereira/Downloads";
+        String filepath = "/home/joel-pereira/IdeaProjects/Java_exercises/retro.wav";
+        File file = new File(filepath);
+
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio not supported");
+        } catch (IOException | LineUnavailableException e) {
+            System.out.println("Something went wrong");
+        }
 
     }
 }
