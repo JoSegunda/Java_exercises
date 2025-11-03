49a1e473bcb7b66bb1e81ad23fd7d4d48ab4def7
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-03 13:24:35 +0000

    updated exe015

--- Exercises/exe015.java
+++ Exercises/exe015.java
@@ -2,6 +2,7 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class exe015 {
@@ -10,13 +11,26 @@ public class exe015 {
         String filepath = "/home/joel-pereira/IdeaProjects/Java_exercises/retro.wav";
         File file = new File(filepath);
 
-        try (Scanner sc = new Scanner(System.in)) {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-
+        try (Scanner sc = new Scanner(System.in);
+             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
+            String response = " ";
+            while (!response.equals("q")){
+                System.out.println("P = play\nS = Stop\n");
+                System.out.println("R = Reset\nQ = Quit\n");
+                System.out.println("Enter your choice: ");
+                response = sc.next().toUpperCase();
 
-            clip.start();
+                switch (response){
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+
 
         } catch (FileNotFoundException e) {
             System.out.println("File not located");
@@ -26,8 +40,7 @@ public class exe015 {
             System.out.println("Something went wrong");
         } finally {
             System.out.println("Bye");
-
         }
-        sc.close();
+
     }
 }
